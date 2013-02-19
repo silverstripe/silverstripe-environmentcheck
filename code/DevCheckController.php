049b3fca060990d027cd419435792faa68262a8a
Julian Seidenberg <julian@silverstripe.com>
2013-02-20 12:14:21 +1300

    BUG adding allowed_actions to all health checks

--- code/DevCheckController.php
+++ code/DevCheckController.php
@@ -1,6 +1,11 @@
 <?php
 
 class DevCheckController extends Controller {
+
+	public static $allowed_actions = array(
+		'index'
+	);
+
 	function index() {
 		$e = new EnvironmentChecker('check', 'Environment status');
 		$e->init('ADMIN');  //check for admin permissions before running this check
--- code/DevHealthController.php
+++ code/DevHealthController.php
@@ -1,6 +1,11 @@
 <?php
 
 class DevHealthController extends Controller {
+
+	public static $allowed_actions = array(
+		'index'
+	);
+
 	function index() {
 		$e = new EnvironmentChecker('health', 'Site health');
 		$e->init('');   //empty permission check, the "health" check does not require a permission check to run
