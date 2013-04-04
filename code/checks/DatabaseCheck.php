a041a6b6437398587a21c5bf8268f2c3bd3a048f
Filype Pereira <filype.pereira@careers.govt.nz>
2013-04-04 22:28:33 +1300

    ENHANCEMENT DatabaseCheck returns a PHP error if the table doesn't exist (fixes #6)

--- code/checks/DatabaseCheck.php
+++ code/checks/DatabaseCheck.php
@@ -5,7 +5,8 @@
 
 
 /**
- * Check that the connection to the database is working, by looking for records in some table.
+ * Check that the connection to the database is working, by ensuring that the table exists and that
+ * the table contain some records.
  * By default, Member will be checked.
  * 
  * @param $checkTable The table that will be checked.
@@ -18,6 +19,11 @@ class DatabaseCheck implements EnvironmentCheck {
 	}
 	
 	function check() {
+
+		if(!DB::getConn()->hasTable($this->checkTable)) {
+			return array(EnvironmentCheck::ERROR, "$this->checkTable not present in the database");
+		}
+
 		$count = DB::query("SELECT COUNT(*) FROM \"$this->checkTable\"")->value();
 		
 		if($count > 0) {
