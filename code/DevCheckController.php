2c0c77df134c01a8f228ef8bd4ae5df9c79880f0
Sean Harvey <sean@silverstripe.com>
2015-04-09 11:39:34 +1200

    Allow specifying the permission for dev/check

--- code/DevCheckController.php
+++ code/DevCheckController.php
@@ -6,10 +6,15 @@ class DevCheckController extends Controller {
 		'index'
 	);
 
+	/**
+	 * @var string Permission code to check for access to this controller.
+	 */
+	private static $permission = 'ADMIN';
+
 	function index($request) {
 		$suiteName = $request->param('Suite') ? $request->param('Suite') : 'check';
 		$e = new EnvironmentChecker($suiteName, 'Environment status');
-		$e->init('ADMIN');  //check for admin permissions before running this check
+		$e->init($this->config()->permission);  //check for admin permissions before running this check
 		return $e;
 	}
 }
