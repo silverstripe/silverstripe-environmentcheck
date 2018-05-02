250c8d2e00c8a11cf9dd655ce5febdff101023be
Robbie Averill <robbie@silverstripe.com>
2018-05-03 10:57:34 +1200

    Use test database in database check test

--- tests/checks/DatabaseCheckTest.php
+++ tests/checks/DatabaseCheckTest.php
@@ -4,6 +4,8 @@
  * @mixin PHPUnit_Framework_TestCase
  */
 class DatabaseCheckTest extends SapphireTest {
+    protected $usesDatabase = true;
+
 	public function testCheckReportsValidConnection() {
 		$check = new DatabaseCheck();
 
