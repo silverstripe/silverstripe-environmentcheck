e323674adfdf93e40ef182767adb3bf58e98ff3b
Frank Mullenger <frank@silverstripe.com>
2019-02-15 11:30:51 +1300

    NEW: Adding Guzzle dependency.

---
+++ src/Traits/Fetcher.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SilverStripe\EnvironmentCheck\Traits;
+
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Simple helper for fetching responses using Guzzle client.
+ *
+ * @package environmentcheck
+ */
+trait Fetcher
+{
+    /**
+     * Configuration for the Guzzle client
+     *
+     * @var array
+     */
+    protected $clientConfig = [];
+
+    /**
+     * Merges configuration arrays and returns the result
+     *
+     * @param array $extraConfig
+     * @return array
+     */
+    private function getClientConfig(array $extraConfig = []) : array
+    {
+        return array_merge($this->clientConfig, $extraConfig);
+    }
+
+    /**
+     * Fetch a response for a URL using Guzzle client.
+     *
+     * @param string $url
+     * @param array|null $extraConfig Extra configuration
+     * @return ResponseInterface
+     */
+    public function fetchResponse(string $url, ? array $extraConfig = []) : ? ResponseInterface
+    {
+        $config = $this->getClientConfig($extraConfig);
+        $client = new Client($config);
+        return $client->get($url);
+    }
+}
